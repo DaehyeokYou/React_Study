bad3a6a4af4ab8a26d460f50cef449ef272f1558
dh-you <dh-you@YDH>
2019-11-20 17:54:43 +0900

    files cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import FuncMyName from './components/FuncMyNameCompo'
 import ClsMyName from './components/ClsMyNameCompo'
+import Counter from './components/CounterCompo'
+import LifeCycle from './components/LifeCycleCompo'
 class App extends Component {
   render() {
     return (
       <div>
-      <FuncMyName/>
-      <br></br>
+      <h1>1. Component 생성 방법</h1>
+      <FuncMyName name='deliveredProps'/>
       <ClsMyName/>
+      <br></br>
+      <hr></hr>
+      <Counter/>
+      <br></br>
+      <hr></hr>
+      <LifeCycle/>
       </div>
     );
   }
