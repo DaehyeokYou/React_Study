92c02929862eb04eda9538e453b313d16a6ce109
dh-you <dh-you@YDH>
2019-11-20 11:24:02 +0900

    Class & Functional Component Create

---
+++ src/components/ClsMyNameCompo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class MyName extends Component {
+  /*
+  // defaultProps 설정 (1)
+  static defaultProps = {
+    name: '기본이름'
+  }
+  */
+  render() {
+    return (
+      <div>
+        안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다. (클래스 컴포넌트)
+      </div>
+    );
+  }
+}
+
+// defaultProps 설정 (2)
+MyName.defaultProps = {
+  name: '기본이름'
+};
+
+export default MyName;
---
+++ src/components/FuncMyNameCompo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+// 함수형 컴포넌트 (1)
+function MyName(props) {
+  return (
+    <div>
+      안녕하세요! 제 이름은 <b>{props.name}</b> 입니다. (함수형 컴포넌트)
+    </div>
+  );
+};
+
+/*
+// 함수형 컴포넌트 (2)
+const MyName = (props) => {
+  return (
+    <div>
+      안녕하세요! 제 이름은 <b>{props.name}</b> 입니다. (함수형 컴포넌트)
+    </div>
+  );
+};
+*/
+/*
+// 함수형 컴포넌트 (3)
+const MyName = ({ name }) => {
+  return (
+    <div>
+      안녕하세요! 제 이름은 <b>{name}</b> 입니다. (함수형 컴포넌트)
+    </div>
+  );
+};
+*/
+
+// defaultProps 설정
+MyName.defaultProps = {
+  name: '기본이름'
+};
+
+
+export default MyName;
